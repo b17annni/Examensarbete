d608c3e1f6a758490b92b44477d03f14b5f97e7e
b17annni <b17annni@student.his.se>
2020-03-28 14:19:20 +0100

    adding reserobot

--- Greger/grgjs.js
+++ Greger/grgjs.js
@@ -1,10 +1,34 @@
-    function getlist(){   
-        $.ajax({
-            url: 'https://api.resrobot.se/v2/location.nearbystops?key=808aabc0-1955-4159-b21c-46b52d70dce1&originCoordLat=59.329630&originCoordLong=18.059338&format=xml',
-            success: writelist,
-          });
+
+    
+    function getlist(){  
+      const section = document.querySelector('section');
+
+      let requestURL = 'https://api.resrobot.se/v2/location.nearbystops?key='+api_key+'&originCoordLat=57.711111&originCoordLong=11.964167&format=json';
+      let request = new XMLHttpRequest();        
+      request.open('GET', requestURL);
+      request.responseType = 'json';
+      request.send();
+
+      request.onload = function() {
+        const locations = request.response;
+        showLocations(locations);
+      }
 };
 
-    function writelist(){
-        
-    }     
+      function showLocations(jsonObj) {
+        const places = jsonObj['StopLocation'];
+            
+        for (let i = 0; i < places.length; i++) {
+          const newart = document.createElement('article');
+          const stationName = document.createElement('h2');
+          const stationDist = document.createElement('p');
+
+          stationName.textContent = places[i].name;
+          stationDist.textContent = 'Distance: ' + places[i].dist;
+
+          newart.appendChild(stationName);
+          newart.appendChild(stationDist);
+ 
+          section.appendChild(newart);
+        }
+      }
