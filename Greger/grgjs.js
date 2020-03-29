2a88d2e9ba78ea7b35a77217ec59bde7f49e5125
b17annni <b17annni@student.his.se>
2020-03-29 21:19:51 +0200

    adding search functions

--- Greger/grgjs.js
+++ Greger/grgjs.js
@@ -1,21 +1,25 @@
+ // getlist = gets locations from input koordinates
+ function getlist(){  
 
-    
-    function getlist(){  
-      const section = document.querySelector('section');
+      var lat = document.getElementById("originCoordLat").value;
+      var long = document.getElementById("originCoordLong").value;
 
-      let requestURL = 'https://api.resrobot.se/v2/location.nearbystops?key='+api_key+'&originCoordLat=57.711111&originCoordLong=11.964167&format=json';
+      let requestURL = 'https://api.resrobot.se/v2/location.nearbystops?key=<Key>&originCoordLat=' + lat + '&originCoordLong=' + long + '&format=json';
       let request = new XMLHttpRequest();        
       request.open('GET', requestURL);
       request.responseType = 'json';
       request.send();
 
       request.onload = function() {
-        const locations = request.response;
-        showLocations(locations);
+       const locations = request.response;
+       showLocations(locations);
       }
 };
-
+// showlocations = writes the locations in a list
       function showLocations(jsonObj) {
+        var list = document.getElementById("tryme");
+        
+
         const places = jsonObj['StopLocation'];
             
         for (let i = 0; i < places.length; i++) {
@@ -25,10 +29,56 @@
 
           stationName.textContent = places[i].name;
           stationDist.textContent = 'Distance: ' + places[i].dist;
-
+      
           newart.appendChild(stationName);
           newart.appendChild(stationDist);
  
-          section.appendChild(newart);
+          list.appendChild(newart);
+        
         }
       }
+
+      function getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(getlistPos);
+        } else { 
+          x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+      }
+      function getlistPos(position){  
+
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+  
+        let requestURL = 'https://api.resrobot.se/v2/location.nearbystops?key=<key>&originCoordLat=' + lat + '&originCoordLong=' + long + '&format=json';
+        let request = new XMLHttpRequest();        
+        request.open('GET', requestURL);
+        request.responseType = 'json';
+        request.send();
+  
+        request.onload = function() {
+         const locations = request.response;
+         showLocations(locations);
+        }
+  };
+  
+        function showLocationsPos(jsonObj) {
+          var list = document.getElementById("tryme");
+  
+          const places = jsonObj['StopLocation'];
+              
+          for (let i = 0; i < places.length; i++) {
+            const newart = document.createElement('article');
+            const stationName = document.createElement('h2');
+            const stationDist = document.createElement('p');
+  
+            stationName.textContent = places[i].name;
+            stationDist.textContent = 'Distance: ' + places[i].dist;
+        
+            newart.appendChild(stationName);
+            newart.appendChild(stationDist);
+   
+            list.appendChild(newart);
+          
+          }
+        }
