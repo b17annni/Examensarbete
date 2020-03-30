d8b8250bd0ecd0609751deff761fdc274dce94be
b17annni <b17annni@student.his.se>
2020-03-31 01:56:28 +0200

    fixing minor red

--- rnGreger/App.js
+++ rnGreger/App.js
@@ -7,22 +7,23 @@ export default class App extends React.Component {
     this.state = {
       isloading: true,
       dataSource: null,
-    }
+    };
   }
 
   componentDidMount() {
     return fetch(
-      'https://api.resrobot.se/v2/location.nearbystops?key=<key>&originCoordLat=59.293525&originCoordLong=18.083519&maxNo=5&format=json')
-      .then((response) => response.json())
-      .then((responseJson) => {
+      'https://api.resrobot.se/v2/location.nearbystops?key=808aabc0-1955-4159-b21c-46b52d70dce1&originCoordLat=59.293525&originCoordLong=18.083519&maxNo=5&format=json',
+    )
+      .then(response => response.json())
+      .then(responseJson => {
         this.setState({
           isloading: false,
           dataSource: responseJson.StopLocation,
-        })
+        });
       })
 
-      .catch((error) => {
-        console.log(error)
+      .catch(error => {
+        console.log(error);
       });
   }
   render() {
@@ -32,18 +33,17 @@ export default class App extends React.Component {
           <ActivityIndicator />
           <Text style={styles.text}> This text </Text>
         </View>
-      )
+      );
     } else {
       let places = this.state.dataSource.map((val, key) => {
-        return <View key={key} style={styles.keystyle}> 
-        <Text style={styles.text}>{val.name}</Text>
+        return (
+          <View key={key} style={styles.keystyle}>
+            <Text style={styles.text}>{val.name}</Text>
           </View>
-        });
+        );
+      });
 
-      return ( 
-      <View style={styles.container}>
-         {places}
-          </View> );
+      return <View style={styles.container}>{places}</View>;
     }
   }
 }
