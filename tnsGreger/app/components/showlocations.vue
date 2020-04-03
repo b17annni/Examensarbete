4ac428284149d26c65ab0dbd139ff6691f87ddcc
b17annni <b17annni@student.his.se>
2020-04-03 02:23:38 +0200

    adding http

--- tnsGreger/app/components/showlocations.vue
+++ tnsGreger/app/components/showlocations.vue
@@ -2,13 +2,21 @@
     <Page actionBarHidden="true">
            <StackLayout>
             <label text="Gregers-FaraHärIfrån"></label>
+             <ListView for="p in location" class="list-group">
+                <v-template>
+                    <StackLayout class="list-group-item">
+                        <Label :text="p.name" />
+                    </StackLayout>
+                </v-template>
+            </ListView>
             <button text="Back" @tap="GetHome" />
         </StackLayout>
     </Page>
 </template>
 
 <script>
-import HomePage from './HomePage';
+  import HomePage from './HomePage';
+  import * as http from "http";
 export default {
     components: {
         HomePage
@@ -17,6 +25,19 @@ export default {
         GetHome(args) {
             this.$navigateTo(HomePage);
         }
-    }
+    },
+     data() {
+            return {
+                location: []
+            };
+        },
+        mounted() { 
+            http.getJSON("https://api.resrobot.se/v2/location.nearbystops?key=<KEY>&originCoordLat=57.711111&originCoordLong=11.964167&maxNo=5&format=json").then(result => {
+    this.location = result.StopLocations;
+}, error => {
+    console.log(error);
+});
+        }
+    
 };
 </script>
